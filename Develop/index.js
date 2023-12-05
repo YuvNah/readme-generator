091800fc0d8ea181a7d890ae30b5141f8b0b19ed
Yuval Nahmias <yuvalit789@gmail.com>
2023-12-05 11:06:53 -0500

    main reademe generater function works, missing questions and styling

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,42 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Name your file",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "add description",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("success")
+  );
+  //   console.log(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    //   .then((answers) => {
+    .then(function (answers) {
+      //   console.log(answers);
+      const readMeContent = generateMarkdown(answers);
+      //   console.log(readMeContent);
+      writeToFile("./utils/README.md", readMeContent);
+    });
+}
 
 // Function call to initialize app
 init();
