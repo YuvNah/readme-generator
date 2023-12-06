4cf186c171ea8637dbb24016a892b09237eb418f
Yuval Nahmias <yuvalit789@gmail.com>
2023-12-05 22:07:04 -0500

    almost done license function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,15 +4,15 @@ function renderLicenseBadge(license) {
   if (license) {
     switch (license) {
       case "MIT":
-        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
       case "Apache 2.0":
-        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]";
       case "Boost":
-        return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+        return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]";
       case "BSD 3-Clause License":
-        return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+        return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]";
       case "BSD 2-Clause License":
-        return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+        return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]";
       default:
         return "";
     }
@@ -20,14 +20,39 @@ function renderLicenseBadge(license) {
     return "";
   }
 }
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    switch (license) {
+      case "MIT":
+        return "(https://opensource.org/licenses/MIT)";
+      case "Apache 2.0":
+        return "(https://opensource.org/licenses/Apache-2.0)";
+      case "Boost":
+        return "(https://www.boost.org/LICENSE_1_0.txt)";
+      case "BSD 3-Clause License":
+        return "(https://opensource.org/licenses/BSD-3-Clause)";
+      case "BSD 2-Clause License":
+        return "(https://opensource.org/licenses/BSD-2-Clause)";
+      default:
+        return "";
+    }
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const badge = renderLicenseBadge(license);
+  const link = renderLicenseLink(license);
+
+  if (badge && link) {
+    return "## License ## License\n${badge}${link}";
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
